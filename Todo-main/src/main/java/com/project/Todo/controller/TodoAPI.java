35740663f344db2a5eb4c429a7400f418b217166
Tất Bảo <ttinhbao123@gmail.com>
2025-03-06 14:06:11 +0700

    Update TodoAPI.java

--- Todo-main/src/main/java/com/project/Todo/controller/TodoAPI.java
+++ Todo-main/src/main/java/com/project/Todo/controller/TodoAPI.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/todo/")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TodoAPI {
     private final TodoService todoService;
 
